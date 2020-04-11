3a1cabf5736af8a27af42aca0026340c48e67103
vampire16 <hakevaibhav16@gmail.com>
2020-04-11 13:53:00 +0530

    ADDED : New test method to check 0 feet and 0 feet should return equal

--- src/test/java/QuantityMeasurementTestCases.java
+++ src/test/java/QuantityMeasurementTestCases.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class QuantityMeasurementTestCases {
+    QuantityMeasurement quantityMeasurement = null;
+
+    @Before
+    public void setUp() {
+        quantityMeasurement = new QuantityMeasurement();
+    }
+
+    @Test
+    public void givenLengthInFeet_ShouldReturnTrue() {
+        Length lengthOne = new Length(0, QuantityMeasurement.Unit.FEET);
+        Length lengthTwo = new Length(0, QuantityMeasurement.Unit.FEET);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertTrue(temp);
+    }
 }
