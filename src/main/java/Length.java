b7b35096dad0a8267c5f3c412503f55b5851a160
vampire16 <hakevaibhav16@gmail.com>
2020-04-11 13:44:39 +0530

    ADDED : Length class to define specific length of unit

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Length {
+
+    private int length;
+    private QuantityMeasurement.Unit unit;
+
+    public Length(int length, QuantityMeasurement.Unit unit) {
+        if(unit == QuantityMeasurement.Unit.FEET){
+            this.length = 12*length;
+            this.unit = QuantityMeasurement.Unit.INCH;
+        } else {
+            this.length = length;
+            this.unit = unit;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length1 = (Length) o;
+        return length == length1.length &&
+                unit == length1.unit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length, unit);
+    }
 }
