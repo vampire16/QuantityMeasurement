840dbd0279b41f251f502443981081efcadfa7c8
vampire16 <hakevaibhav16@gmail.com>
2020-04-12 19:47:46 +0530

    ADDED : New enum class for different units

--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public enum Unit {
+    FEET(12),
+    INCH(1),
+    YARD(36);
+
+    private final double length;
+
+    Unit(double length) { this.length = length; }
+
+    public double getValue() { return length; }
 }
