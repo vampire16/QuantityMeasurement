dff78ca304f446ba6d28fb656ffbf7a2881f19fa
vampire16 <hakevaibhav16@gmail.com>
2020-04-12 19:45:45 +0530

    MODIFIED : Removed if statement in compare method

--- src/main/java/QuantityMeasurement.java
+++ src/main/java/QuantityMeasurement.java
@@ -1,17 +1,10 @@
 public class QuantityMeasurement {
 
-    enum Unit{
-        FEET,
-        INCH
-    }
-
     public static void main(String[] args){
         System.out.println("Welcome");
     }
 
     public boolean compare(Length lengthOne, Length lengthTwo){
-        if(lengthOne.equals(lengthTwo))
-            return  true;
-        return false;
+        return lengthOne.equals(lengthTwo);
     }
 }
