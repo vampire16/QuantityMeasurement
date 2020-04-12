f8ae26c7f51d3bb2aed976fe74859b6cb2a3e1a8
vampire16 <hakevaibhav16@gmail.com>
2020-04-12 19:48:44 +0530

    ADDED : New test methods

--- src/test/java/QuantityMeasurementTestCases.java
+++ src/test/java/QuantityMeasurementTestCases.java
@@ -12,15 +12,15 @@ public class QuantityMeasurementTestCases {
 
     @Test
     public void givenLengthInFeet_ShouldReturnTrue() {
-        Length lengthOne = new Length(0, QuantityMeasurement.Unit.FEET);
-        Length lengthTwo = new Length(0, QuantityMeasurement.Unit.FEET);
+        Length lengthOne = new Length(0, Unit.FEET);
+        Length lengthTwo = new Length(0, Unit.FEET);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenLengthInFeetAndNull_ShouldReturnFalse() {
-        Length lengthOne = new Length(0, QuantityMeasurement.Unit.FEET);
+        Length lengthOne = new Length(0, Unit.FEET);
         Length lengthTwo = null;
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertFalse(temp);
@@ -28,46 +28,46 @@ public class QuantityMeasurementTestCases {
 
     @Test
     public void givenSameFeetRef_ShouldReturnTrue() {
-        Length length = new Length(0, QuantityMeasurement.Unit.FEET);
+        Length length = new Length(0, Unit.FEET);
         boolean temp = quantityMeasurement.compare(length, length);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenSameFeetType_ShouldReturnTrue() {
-        Length lengthOne = new Length(0, QuantityMeasurement.Unit.FEET);
-        Length lengthTwo = new Length(0, QuantityMeasurement.Unit.FEET);
+        Length lengthOne = new Length(0, Unit.FEET);
+        Length lengthTwo = new Length(0, Unit.FEET);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenSameFeetValue_ShouldReturnTrue() {
-        Length lengthOne = new Length(5, QuantityMeasurement.Unit.FEET);
-        Length lengthTwo = new Length(5, QuantityMeasurement.Unit.FEET);
+        Length lengthOne = new Length(5, Unit.FEET);
+        Length lengthTwo = new Length(5, Unit.FEET);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenFeetDifferentValue_ShouldReturnTrue() {
-        Length lengthOne = new Length(4, QuantityMeasurement.Unit.FEET);
-        Length lengthTwo = new Length(5, QuantityMeasurement.Unit.FEET);
+        Length lengthOne = new Length(4, Unit.FEET);
+        Length lengthTwo = new Length(5, Unit.FEET);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertFalse(temp);
     }
 
     @Test
     public void givenLengthInInch_ShouldReturnTrue() {
-        Length lengthOne = new Length(0, QuantityMeasurement.Unit.INCH);
-        Length lengthTwo = new Length(0, QuantityMeasurement.Unit.INCH);
+        Length lengthOne = new Length(0, Unit.INCH);
+        Length lengthTwo = new Length(0, Unit.INCH);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenLengthInInchAndNull_ShouldReturnFalse() {
-        Length lengthOne = new Length(0, QuantityMeasurement.Unit.INCH);
+        Length lengthOne = new Length(0, Unit.INCH);
         Length lengthTwo = null;
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertFalse(temp);
@@ -75,39 +75,87 @@ public class QuantityMeasurementTestCases {
 
     @Test
     public void givenSameInchRef_ShouldReturnTrue() {
-        Length length = new Length(0, QuantityMeasurement.Unit.INCH);
+        Length length = new Length(0, Unit.INCH);
         boolean temp = quantityMeasurement.compare(length, length);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenSameInchType_ShouldReturnTrue() {
-        Length lengthOne = new Length(0, QuantityMeasurement.Unit.INCH);
-        Length lengthTwo = new Length(0, QuantityMeasurement.Unit.INCH);
+        Length lengthOne = new Length(0, Unit.INCH);
+        Length lengthTwo = new Length(0, Unit.INCH);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenSameInchValue_ShouldReturnTrue() {
-        Length lengthOne = new Length(5, QuantityMeasurement.Unit.INCH);
-        Length lengthTwo = new Length(5, QuantityMeasurement.Unit.INCH);
+        Length lengthOne = new Length(5, Unit.INCH);
+        Length lengthTwo = new Length(5, Unit.INCH);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
 
     @Test
     public void givenInchDifferentValue_ShouldReturnTrue() {
-        Length lengthOne = new Length(4, QuantityMeasurement.Unit.INCH);
-        Length lengthTwo = new Length(5, QuantityMeasurement.Unit.INCH);
+        Length lengthOne = new Length(4, Unit.INCH);
+        Length lengthTwo = new Length(5, Unit.INCH);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertFalse(temp);
     }
 
     @Test
     public void givenFeetAndInch_ShouldReturnTrue() {
-        Length lengthOne = new Length(1, QuantityMeasurement.Unit.FEET);
-        Length lengthTwo = new Length(12, QuantityMeasurement.Unit.INCH);
+        Length lengthOne = new Length(1, Unit.FEET);
+        Length lengthTwo = new Length(12, Unit.INCH);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertTrue(temp);
+    }
+
+    @Test
+    public void given3FeetAnd1Yard_ShouldReturnTrue() {
+        Length lengthOne = new Length(3, Unit.FEET);
+        Length lengthTwo = new Length(1, Unit.YARD);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertTrue(temp);
+    }
+
+    @Test
+    public void given1FeetAnd1Yard_ShouldReturnTrue() {
+        Length lengthOne = new Length(1, Unit.FEET);
+        Length lengthTwo = new Length(1, Unit.YARD);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertFalse(temp);
+    }
+
+    @Test
+    public void given1InchAnd1Yard_ShouldReturnTrue() {
+        Length lengthOne = new Length(1, Unit.INCH);
+        Length lengthTwo = new Length(1, Unit.YARD);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertFalse(temp);
+    }
+
+    @Test
+    public void given1YardAnd36Inch_ShouldReturnTrue() {
+        Length lengthOne = new Length(1, Unit.YARD);
+        Length lengthTwo = new Length(36, Unit.INCH);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertTrue(temp);
+    }
+
+    @Test
+    public void given36InchAnd1Yard_ShouldReturnTrue() {
+        Length lengthOne = new Length(36, Unit.INCH);
+        Length lengthTwo = new Length(1, Unit.YARD);
+        boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
+        Assert.assertTrue(temp);
+    }
+
+    @Test
+    public void given1YardAnd3Feet_ShouldReturnTrue() {
+        Length lengthOne = new Length(1, Unit.YARD);
+        Length lengthTwo = new Length(3, Unit.FEET);
         boolean temp = quantityMeasurement.compare(lengthOne, lengthTwo);
         Assert.assertTrue(temp);
     }
