b25b1b8209d7822f5e5344301cb26a80791f0850
vampire16 <hakevaibhav16@gmail.com>
2020-04-13 06:53:37 +0530

    REFACTORED : Gradle files

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -10,16 +10,13 @@ public class Length {
         this.unit = unit;
     }
 
+    public double getValue() { return length; }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Length length1 = (Length) o;
         return Double.compare(length1.length, length) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(length, unit);
     }
 }
