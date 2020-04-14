77d2cdb38ee176818aa7fc6b33b5e8396ad31729
vampire16 <hakevaibhav16@gmail.com>
2020-04-14 10:59:47 +0530

    REFACTORED : EntityType added in Entity parameter

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,16 +1,16 @@
 public enum Entity {
-    FEET(12,EntityType.LENGTH),
-    INCH(1,EntityType.LENGTH),
-    YARD(36,EntityType.LENGTH),
-    CENTIMETER(0.4,EntityType.LENGTH),
-    LITRE(1,EntityType.VOLUME),
-    MILLILITER(0.001,EntityType.VOLUME),
-    GALLON(3.78,EntityType.VOLUME),
-    GRAM(0.001,EntityType.WEIGHT),
-    KILOGRAM(1,EntityType.WEIGHT),
-    TONNE(1000,EntityType.WEIGHT),
-    FAHRENHEIT(1,EntityType.TEMPERATURE),
-    CELSIUS(1,EntityType.TEMPERATURE);
+    FEET(12, EntityType.LENGTH),
+    INCH(1, EntityType.LENGTH),
+    YARD(36, EntityType.LENGTH),
+    CENTIMETER(0.4, EntityType.LENGTH),
+    LITRE(1, EntityType.VOLUME),
+    MILLILITER(0.001, EntityType.VOLUME),
+    GALLON(3.78, EntityType.VOLUME),
+    GRAM(0.001, EntityType.WEIGHT),
+    KILOGRAM(1, EntityType.WEIGHT),
+    TONNE(1000, EntityType.WEIGHT),
+    FAHRENHEIT(1, EntityType.TEMPERATURE),
+    CELSIUS(1, EntityType.TEMPERATURE);
 
     private final double value;
     EntityType entityType;
